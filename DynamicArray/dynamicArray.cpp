3c712ce40aace7b9781a0a3673bc66f14ad5e42e
fdelaney <delaneyfrancis@hotmail.com>
2015-01-30 10:30:29 -0500

    Test 1 et 2 passent

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,1 +1,27 @@
 #include "dynamicArray.h"
+
+DynamicArray::DynamicArray(int _capacite)
+{
+	this->capacite = _capacite;
+	this->tabElements = new int [_capacite]{};
+}
+
+int DynamicArray::getCapacite()
+{
+	return capacite;
+}
+
+void DynamicArray::setCapacite(int _capacite)
+{
+	capacite = _capacite;
+}
+
+void DynamicArray::setElement(unsigned int _index, int _valeur)
+{
+	tabElements[_index] = _valeur;
+}
+
+int DynamicArray::getElement(unsigned int _index)
+{
+	return tabElements[_index];
+}
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -6,12 +6,15 @@ class DynamicArray
 {
 
 public:
-
-
+	DynamicArray(int _capacite = 100);
+	int getCapacite();
+	void setCapacite(int _capacite);
+	void setElement(unsigned int _index, int _valeur);
+	int getElement(unsigned int _index);
 
 private:
-
-
+	int capacite;
+	int* tabElements;
 };
 
 
